cabbbaab928e16dfc5685580acb1f6553889042a
h <aahasib.aust@gmail.com>
2021-09-29 14:33:13 +0600

    Learn Laravel

---
+++ WebApplication1/Controllers/DeveloperLoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Web.Security;
+using System.Data.SqlClient;
+
+
+namespace WebApplication1.Controllers
+{
+    public class DeveloperLoginController : Controller
+    {
+        public ActionResult DeveloperLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeveloperLogin(DEVELOPER objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DBmodelEntities db = new DBmodelEntities())
+                {
+                    var obj = db.DEVELOPERS.Where(a => a.DeveloperEmail.Equals(objUser.DeveloperEmail) && a.DeveloperPass.Equals(objUser.DeveloperPass)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["DeveloperEmail"] = objUser.DeveloperEmail.ToString();
+                         //< a  href="~/DeveloperLogin/DeveloperLogin">Developer Login</a>
+                        return RedirectToAction("DeveloperLogin", "DeveloperLogin");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        
+    }
+}
---
+++ WebApplication1/Models/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Contract
+    {
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID required")]
+        public string EmailID { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
